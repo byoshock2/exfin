65ce1604d881a2935ed7f54ba4cf6ad7758ab881
byoshock2 <leyendo31@gmail.com>
2017-11-21 13:19:07 -0600

    Ya terminado

--- materia/models.py
+++ materia/models.py
@@ -4,16 +4,10 @@ from django.db import models
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
-def __str__(self):
-    return self.title
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50)
+    profesor = models.CharField(max_length=200)
+    nota = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
--- materia/views.py
+++ materia/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Materia
+from django.shortcuts import render, get_object_or_404
+from .forms import MateriaForm
+from django.shortcuts import redirect
 
-# Create your views here.
+def materia_new(request):
+    if request.method == "POST":
+        form = MateriaForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+        return render(request, 'materia/materia_edit.html', {'form': form})
+
+    else:
+        form = MateriaForm()
+        return render(request, 'materia/materia_edit.html', {'form': form})
